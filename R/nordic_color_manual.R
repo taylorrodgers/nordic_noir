6701f8f2b89945dc8e94a9e4bcec9786a0bb9d78
Taylor Rodgers <taylor@taylorrodgers.com>
2023-09-24 13:13:52 -0500

    Nordic noir
    Initial commit

---
+++ R/nordic_color_manual.R
@@ -0,0 +1,8 @@
+#' Update color to use nordic noir color palette.
+#'
+#' @return Updates colors.
+#' @export
+#'
+nordic_color_manual = function(){
+  scale_color_manual(values = c("#A4B5C2ff", "#545343ff", "#2FC1D3", "#353D36ff" ,"#C1D1D3ff", "#748282ff"))
+}
---
+++ R/nordic_fill_manual.R
@@ -0,0 +1,7 @@
+#' Update fill to use nordic noir color palette.
+#'
+#' @return Updates colors.
+#' @export
+nordic_fill_manual = function(){
+  scale_fill_manual(values = c("#A4B5C2ff", "#545343ff", "#2FC1D3", "#353D36ff" ,"#C1D1D3ff", "#748282ff"))
+}
